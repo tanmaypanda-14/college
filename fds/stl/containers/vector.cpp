d0400cdeedf850d19a06e84d448b29bedad6c290
uneconomicalfairy14 <tanmay404103@outlook.com>
2022-09-18 09:21:59 +0530

    9:21

--- fds/stl/containers/vector.cpp
+++ fds/stl/containers/vector.cpp
@@ -5,21 +5,28 @@ using namespace std;
 
 int main() {
   vector<int> v;
-  int n,temp;
+  int n, temp;
 
-  cout<<"Enter number of elements "<<endl;
-  cin>>n;
+  cout << "Enter number of elements " << endl;
+  cin >> n;
 
-  for(int i=0;i<n;i++){
-    cin>>temp;
+  for (int i = 0; i < n; i++) {
+    cin >> temp;
     v.push_back(temp);
   }
+  cout<<endl;
 
-
-  for (int i = 0; i < v.size(); i++) {
-    v[i] += 3;
-    cout << v[i] << endl;
+  // iterating using stl
+  vector<int>::iterator it;
+  for (it = v.begin(); it != v.end(); it++) {
+    cout << *it << " ";
   }
+
+  // // default way to iterate vector
+  // for (int i = 0; i < v.size(); i++) {
+  //   // v[i] += 3;
+  //   cout << v[i] << endl;
+  // }
 
   return 0;
 }
