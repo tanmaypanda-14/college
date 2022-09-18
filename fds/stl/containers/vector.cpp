f587a93dba80250053e0cfe3b77deaaa7b2f1050
uneconomicalfairy14 <tanmay404103@outlook.com>
2022-09-18 09:05:43 +0530

    9:05

---
+++ fds/stl/containers/vector.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector<int> v;
+  int n,temp;
+
+  cout<<"Enter number of elements "<<endl;
+  cin>>n;
+
+  for(int i=0;i<n;i++){
+    cin>>temp;
+    v.push_back(temp);
+  }
+
+
+  for (int i = 0; i < v.size(); i++) {
+    v[i] += 3;
+    cout << v[i] << endl;
+  }
+
+  return 0;
+}
